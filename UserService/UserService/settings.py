7755bb89df23915ebf7d435f2db61d80d2ea3bcd
Guejia21 <jonathan.guejia@unicauca.edu.co>
2025-05-22 09:20:06 -0500

    CORS problem fixed

--- UserService/UserService/settings.py
+++ UserService/UserService/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'UserApi'
 ]
 
@@ -58,6 +59,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200", 
 ]
 
 ROOT_URLCONF = 'UserService.urls'
