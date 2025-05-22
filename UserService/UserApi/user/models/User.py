e04b9afab7e4330799f7289e1197f283d1961660
Juliandross <138513671+Juliandross2@users.noreply.github.com>
2025-05-21 21:18:38 -0500

    weba

--- UserService/UserApi/user/models/User.py
+++ UserService/UserApi/user/models/User.py
@@ -22,7 +22,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=30)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    rol = models.CharField(max_length=10, default='DOCENTE')
+    rol = models.CharField(max_length=20, default='DOCENTE')
 
     objects = UserManager()
 
