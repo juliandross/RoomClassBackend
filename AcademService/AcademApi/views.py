14347069e26510609efe56c86c9c8c77b4beaa7e
DavidRMA <165867613+DavidRMA@users.noreply.github.com>
2025-06-02 15:40:27 -0500

    add endpoints

--- AcademService/AcademApi/views.py
+++ AcademService/AcademApi/views.py
@@ -40,10 +40,47 @@ from AcademApi.permissions import IsCoordinator
 class SubjectCompetenceViewSet(viewsets.ModelViewSet):
     queryset = SubjectCompetence.objects.all()
     serializer_class = SubjectCompetenceSerializer
+    
+    # Action to get a Subject Competence with their associated RAs
+    @action(detail=True, methods=['get'], url_path='RA')
+    def subject_competence_with_ra_asociated(self, request, pk=None):
+        try:
+            subject_competence = SubjectCompetence.objects.get(pk=pk)
+        except SubjectCompetence.DoesNotExist:
+            return Response({'error': 'Competencia de asignatura no encontrada.'}, status=404)
+        
+        ras = SubjectRA.objects.filter(subjectCompetence=subject_competence)
+        
+        data = {
+            'SubjectCompetence': SubjectCompetenceSerializer(subject_competence).data,
+            'SubjectRA': SubjectRASerializer(ras, many=True).data
+        }
+        
+        return Response(data)
 
 class SubjectRAViewSet(viewsets.ModelViewSet):
     queryset = SubjectRA.objects.all()
     serializer_class = SubjectRASerializer
+    
+    # Action to get a Subject RA with their associated Rubric
+    @action(detail=True, methods=['get'], url_path='Rubric')
+    def subject_ra_with_rubric_asociated(self, request, pk=None):
+        try:
+            subject_ra = SubjectRA.objects.get(pk=pk)
+        except SubjectRA.DoesNotExist:
+            return Response({'error': 'RA de asignatura no encontrado.'}, status=404)
+        
+        try:
+            rubric = Rubric.objects.get(SubjectRA=subject_ra)
+            rubric_data = RubricSerializer(rubric).data
+        except Rubric.DoesNotExist:
+            return Response({'error': 'No hay ninguna rúbrica asociada al resultado de aprendizaje de asignatura.'}, status=404)
+        
+        data = {
+            'SubjectRA': SubjectRASerializer(subject_ra).data,
+            'Rubric': rubric_data
+        }
+        return Response(data)
     
 
 # Class to handle the direcory Subject 
@@ -105,6 +142,30 @@ class SubjectViewSet(viewsets.ModelViewSet):
             'subject': SubjectSerializer(subject).data,
             'SubjectCompetences': data
         })
+        
+    # Action to get a Subject with their associated program competences
+    @action(detail=True, methods=['get'], url_path='programCompetence')
+    def subject_with_program_competences(self, request, pk=None):
+        try:
+            subject = Subject.objects.get(pk=pk)
+        except Subject.DoesNotExist:
+            return Response({'error': 'Asignatura no encontrada.'}, status=404)
+        
+        # Obtener todos los enlaces intermedios
+        competence_links = CompetenceProgramSubject.objects.filter(subject=subject).select_related('programCompetence')
+        
+        # Serializar solo las competencias (no los enlaces)
+        program_competences_data = [
+            ProgramCompetenceSerializer(link.programCompetence).data
+            for link in competence_links
+        ]
+        
+        return Response({
+            'subject': SubjectSerializer(subject).data,
+            'programCompetences': program_competences_data
+        })
+
+        
 
 class PeriodViewSet(viewsets.ModelViewSet):
     queryset = Period.objects.all()
@@ -113,10 +174,79 @@ class PeriodViewSet(viewsets.ModelViewSet):
 class TeacherViewSet(viewsets.ModelViewSet):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
-
+    
+    # Action to get a Teacher with their associated Subjects
+    @action(detail=True, methods=['get'], url_path='Subject_asociated')
+    def teacher_byId_with_subject_asociated(self, request, pk=None):
+        try:
+            teacher = Teacher.objects.get(pk=pk)
+        except Teacher.DoesNotExist:
+            return Response({'error': 'Docente no encontrado.'}, status=404)
+        
+        subjects = Subject.objects.filter(subject_teacher_periods__teacher=teacher).distinct()
+        
+        subjects_data = []
+        for subject in subjects:
+            subjects_data.append({
+                'Subject': SubjectSerializer(subject).data,
+            })
+        
+        result = {
+            'Teacher': TeacherSerializer(teacher).data,
+            'Subjects': subjects_data
+        }
+        return Response(result)
+            
 class SubjectTeacherPeriodViewSet(viewsets.ModelViewSet):
     queryset = SubjectTeacherPeriod.objects.all()
     serializer_class = SubjectTeacherPeriodSerializer
+    
+    # Action to get a "SubjectTeacherPeriod" with their associated Subjects competences
+    @action(detail=True, methods=['get'], url_path='SubjectCompetence_asociated')
+    def subject_teacher_period_with_competence_asociated(self, request, pk=None):
+        try:
+            subject_teacher_period = SubjectTeacherPeriod.objects.get(pk=pk)
+        except SubjectTeacherPeriod.DoesNotExist:
+            return Response({'error': 'Instancia de "sujec_teacher_period" no encontrada.'}, status=404)
+        
+        competences = SubjectCompetence.objects.filter(subjectTeacherPeriod=subject_teacher_period)
+        
+        competences_data = []
+        for competence in competences:
+            competences_data.append({
+                'SubjectCompetence': SubjectCompetenceSerializer(competence).data,
+            })
+        
+        result = {
+            'SubjectTeacherPeriod': SubjectTeacherPeriodSerializer(subject_teacher_period).data,
+            'SubjectCompetences': competences_data
+        }
+        return Response(result)
+    
+    # Action to get all "SubjectTeacherPeriod" with their associated Subjects competences + RAs
+    @action(detail=True, methods=['get'], url_path='SubjectCompetence/RA')
+    def subject_teacher_period_with_competence_ra_asociated(self, request, pk=None):
+        try:
+            subject_teacher_period = SubjectTeacherPeriod.objects.get(pk=pk)
+        except SubjectTeacherPeriod.DoesNotExist:
+            return Response({'error': 'Instancia de "subject_teacher_period" no encontrada.'}, status=404)
+        
+        competences = SubjectCompetence.objects.filter(subjectTeacherPeriod=subject_teacher_period)
+        
+        competences_data = []
+        for competence in competences:
+            ras = SubjectRA.objects.filter(subjectCompetence=competence)
+            competences_data.append({
+                'SubjectCompetence': SubjectCompetenceSerializer(competence).data,
+                'SubjectRA': SubjectRASerializer(ras, many=True).data
+            })
+        
+        result = {
+            'SubjectTeacherPeriod': SubjectTeacherPeriodSerializer(subject_teacher_period).data,
+            'SubjectCompetences': competences_data
+        }
+        return Response(result)
+    
 
 # Class to handle the directory Comp_RA_Program
 class ProgramCompetenceViewSet(viewsets.ModelViewSet):
